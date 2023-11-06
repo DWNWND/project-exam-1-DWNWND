78ab5c6ad67613799ef4af5dc4951ec0e0b08ee8
Thea <theola51924@stud.noroff.no>
2023-11-06 14:43:09 +0100

    added the navbar to the different pages and made an index-spesific navbar

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,14 @@
 const hamburgerMenu = document.querySelector(".hamburger-toggle");
 const navStand = document.querySelector(".navbar-standard");
 const navCat = document.querySelector(".navbar-categories");
+const headerIndex = document.querySelector(".header-display-index");
+const newsletterIndex = document.querySelector(".newsletter-signup-index");
+const logoIndex = document.querySelector(".logo-mobile-index");
 
 hamburgerMenu.addEventListener("click", () => {
   navStand.classList.toggle("active");
   navCat.classList.toggle("active");
+  headerIndex.classList.toggle("active");
+  newsletterIndex.classList.toggle("active");
+  logoIndex.classList.toggle("active");
 });
