a883ee7681354f954a844fe4a054537e83e9b154
Thea <theola51924@stud.noroff.no>
2023-11-07 18:30:19 +0100

    added a carousel-slider, working on responsiveness of slider

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+// NAV-MENU
 const hamburgerMenu = document.querySelector(".hamburger-toggle");
 const navStand = document.querySelector(".navbar-standard");
 const navCat = document.querySelector(".navbar-categories");
@@ -11,4 +12,21 @@ hamburgerMenu.addEventListener("click", () => {
   headerIndex.classList.toggle("active");
   newsletterIndex.classList.toggle("active");
   logoIndex.classList.toggle("active");
+});
+
+// CAROUSEL
+const prev = document.querySelector(".prev-btn");
+const next = document.querySelector(".next-btn");
+const carouselSlider = document.querySelector(".slider");
+
+//how far it will scroll:
+const itemWidth = 250;
+const padding = 0;
+
+prev.addEventListener("click", () => {
+  carouselSlider.scrollLeft -= itemWidth + padding;
+});
+
+next.addEventListener("click", () => {
+  carouselSlider.scrollLeft += itemWidth + padding;
 });
