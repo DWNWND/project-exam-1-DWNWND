089d1d25955a906357ab2021956703b61ccaeba7
Thea <theola51924@stud.noroff.no>
2023-11-11 13:39:25 +0100

    cleaned up in the css and classes for the header/navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,46 +1,46 @@
-// render header
+// render header/navbar
 const modal = document.querySelector("[data-modal]");
 modal.innerHTML += `
 <a data-close-modal href="#" class="hamburger-toggle closeBtn">
-<i class="fa-solid fa-xmark"></i>
+  <i class="fa-solid fa-xmark"></i>
 </a>
 <div class="logo-mobile">
-<a href="#"><img src="/img/logo-black.png" /></a>
+  <a href="#"><img src="/img/logo-black.png" /></a>
 </div>
 <div class="navbar-standard">
-<ul class="navbar-standard-alignment">
-  <div class="standard-links">
-    <li class="nav-item-stand"><a href="#">Home</a></li>
-    <li class="nav-item-stand"><a href="html/about.html">About</a></li>
-    <li class="nav-item-stand"><a href="html/contact.html">Contact</a></li>
-  </div>
-  <li class="newsletter-signup"><a href="#">newsletter signup</a></li>
-  <div class="icon-links">
-    <li class="nav-icon-stand">
-      <a href="#"><i class="fa-brands fa-pinterest"></i></a>
-    </li>
-    <li class="nav-icon-stand">
-      <a href="#"><i class="fa-brands fa-amazon"></i></a>
-    </li>
-    <li class="nav-icon-stand">
-      <a href="#"><i class="fa-brands fa-youtube"></i></a>
-    </li>
-  </div>
-</ul>
+  <ul class="navbar-standard-alignment">
+    <div class="standard-links">
+      <li class="nav-item-stand"><a href="#">Home</a></li>
+      <li class="nav-item-stand"><a href="html/about.html">About</a></li>
+      <li class="nav-item-stand"><a href="html/contact.html">Contact</a></li>
+   </div>
+   <li class="newsletter-signup"><a href="#">newsletter signup</a></li>
+   <div class="icon-links">
+      <li class="nav-icon-stand">
+        <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+      </li>
+      <li class="nav-icon-stand">
+        <a href="#"><i class="fa-brands fa-amazon"></i></a>
+      </li>
+      <li class="nav-icon-stand">
+        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+      </li>
+    </div>
+  </ul>
 </div>
-<div class="header-display">
-<img src="/img/header-img_unsplash.jpg" class="header-img" />
-<img src="/img/logo-white-big.svg" class="logo-big" />
+<div class="header-general-wrapper">
+  <img src="/img/header-img_unsplash.jpg" class="header-img" />
+  <img src="/img/logo-white-big.svg" class="logo-big" />
 </div>
 <div class="navbar-categories">
-<ul class="navbar-categories-alignement">
-  <li class="nav-item-cat"><a href="/html/list.html">Micro living</a></li>
-  <li class="nav-item-cat"><a href="#">Vanlife</a></li>
-  <li class="nav-item-cat"><a href="#">Boatlife</a></li>
-  <li class="nav-item-cat"><a href="#">Self Reliance</a></li>
-  <li class="nav-item-cat"><a href="#">DIY</a></li>
-  <li class="nav-item-cat"><a href="#">Tinyhouse</a></li>
-</ul>
+  <ul class="navbar-categories-alignment">
+    <li class="nav-item-cat"><a href="/html/list.html">Micro living</a></li>
+    <li class="nav-item-cat"><a href="#">Vanlife</a></li>
+    <li class="nav-item-cat"><a href="#">Boatlife</a></li>
+    <li class="nav-item-cat"><a href="#">Self Reliance</a></li>
+    <li class="nav-item-cat"><a href="#">DIY</a></li>
+    <li class="nav-item-cat"><a href="#">Tinyhouse</a></li>
+  </ul>
 </div>`;
 
 // open hamburger menu
