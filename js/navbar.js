83ddcee81498c193cfec2fe54ca543824a656d9e
Thea <theola51924@stud.noroff.no>
2023-11-11 13:09:21 +0100

    changed the nav-menu to a modal in mobile view and included rendering of the header from js on each page instead of hardcode html

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,65 @@
-// // NAV-MENU
-// const hamburgerMenu = document.querySelector(".hamburger-toggle");
-// const navStand = document.querySelector(".navbar-standard");
-// const navCat = document.querySelector(".navbar-categories");
-// const headerIndex = document.querySelector(".header-display-index");
-// const newsletterIndex = document.querySelector(".newsletter-signup-index");
-// const logoIndex = document.querySelector(".logo-mobile-index");
+// render header
+const modal = document.querySelector("[data-modal]");
+modal.innerHTML += `
+<a data-close-modal href="#" class="hamburger-toggle closeBtn">
+<i class="fa-solid fa-xmark"></i>
+</a>
+<div class="logo-mobile">
+<a href="#"><img src="/img/logo-black.png" /></a>
+</div>
+<div class="navbar-standard">
+<ul class="navbar-standard-alignment">
+  <div class="standard-links">
+    <li class="nav-item-stand"><a href="#">Home</a></li>
+    <li class="nav-item-stand"><a href="html/about.html">About</a></li>
+    <li class="nav-item-stand"><a href="html/contact.html">Contact</a></li>
+  </div>
+  <li class="newsletter-signup"><a href="#">newsletter signup</a></li>
+  <div class="icon-links">
+    <li class="nav-icon-stand">
+      <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+    </li>
+    <li class="nav-icon-stand">
+      <a href="#"><i class="fa-brands fa-amazon"></i></a>
+    </li>
+    <li class="nav-icon-stand">
+      <a href="#"><i class="fa-brands fa-youtube"></i></a>
+    </li>
+  </div>
+</ul>
+</div>
+<div class="header-display">
+<img src="/img/header-img_unsplash.jpg" class="header-img" />
+<img src="/img/logo-white-big.svg" class="logo-big" />
+</div>
+<div class="navbar-categories">
+<ul class="navbar-categories-alignement">
+  <li class="nav-item-cat"><a href="/html/list.html">Micro living</a></li>
+  <li class="nav-item-cat"><a href="#">Vanlife</a></li>
+  <li class="nav-item-cat"><a href="#">Boatlife</a></li>
+  <li class="nav-item-cat"><a href="#">Self Reliance</a></li>
+  <li class="nav-item-cat"><a href="#">DIY</a></li>
+  <li class="nav-item-cat"><a href="#">Tinyhouse</a></li>
+</ul>
+</div>`;
 
-// hamburgerMenu.addEventListener("click", () => {
-//   navStand.classList.toggle("active");
-//   navCat.classList.toggle("active");
-//   headerIndex.classList.toggle("active");
-//   newsletterIndex.classList.toggle("active");
-//   logoIndex.classList.toggle("active");
-// });
-
-// NAV-MENU - made better??
-const hamburgerMenu = document.querySelector(".hamburger-toggle");
-const displayOn = document.querySelectorAll(".on");
-const displayOff = document.querySelectorAll(".off");
-const displayFlex = document.querySelectorAll(".on-ex");
-
-hamburgerMenu.addEventListener("click", () => {
-  displayOn.forEach((element) => {
-    element.classList.toggle("active");
-  });
-  displayOff.forEach((element) => {
-    element.classList.toggle("active");
-  });
-  displayFlex.forEach((element) => {
-    element.classList.toggle("active");
-  });
+// open hamburger menu
+const openBtn = document.querySelector("[data-open-modal]");
+openBtn.addEventListener("click", () => {
+  // modal.style.opacity = "1";
+  modal.showModal();
 });
+
+// close hamburger menu by clicking X
+const closeBtn = document.querySelector("[data-close-modal]");
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
+
+// close hamburger menu by clicking outside the menu
+function onClick(event) {
+  if (event.target === modal) {
+    modal.close();
+  }
+}
+modal.addEventListener("click", onClick);
