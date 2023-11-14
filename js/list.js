96f3859ac75965bb62eedad2db95e973fd6a7b11
Thea <theola51924@stud.noroff.no>
2023-11-14 18:38:50 +0100

    added a timestamp/publish date on the posts

--- js/list.js
+++ js/list.js
@@ -1,49 +1,56 @@
 import { fetchPostsByCategory, fetchSpesificImages, fetchCategory } from "./api-call.js";
 
-//render category name
-
+//render category name/pagetitle
 async function renderCategoryName() {
   const currentCategory = await fetchCategory();
-
-  console.log(currentCategory);
-
   const pageTitle = document.querySelector(".pagetitle");
-
   pageTitle.innerHTML += `${currentCategory.name}`;
 }
-
 renderCategoryName();
 
+//render filepath.....
+
+//render categorizes posts
 async function renderCategoriezedPosts() {
   const allCategorizedPosts = await fetchPostsByCategory();
 
   for (let i = 0; i < allCategorizedPosts.length; i++) {
-    // console.log(allCategorizedPosts[i]);
     const postTitle = allCategorizedPosts[i].title.rendered;
     const excerpt = allCategorizedPosts[i].excerpt.rendered;
 
+    //formatting the date that is displayed on each post
+    const initialWpPublishedDate = allCategorizedPosts[i].date;
+    const formattableDate = new Date(initialWpPublishedDate).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+    const dateArr = formattableDate.split(",");
+    const displayedDate = dateArr[0];
+    const displayedYearAndTime = dateArr[1];
+
+    //formatting the linked imageurl for each post
     const imageApi = allCategorizedPosts[i]._links["wp:featuredmedia"]["0"].href;
     const img = await fetchSpesificImages(imageApi);
     const featuredImg = img.source_url;
     const altText = img.alt_text;
 
+    //render article
     const displayCategorizedPosts = document.querySelector(".categorized-posts");
-
     displayCategorizedPosts.innerHTML += `
     <article>
       <h2>${postTitle}</h2>
       <p>${excerpt}</p>
       <figure><img src="${featuredImg}" alt="${altText}" /></figure>
       <div class="post-info">
-        <div class="publish-date">published: <date>11.11.2023</date></div>
-        <a href="#" class="continue-btn">continue reading...</a>
+        <div class="publish-date"><date>${displayedDate}, ${displayedYearAndTime}</date></div>
+        <a href="/html/post.html${allCategorizedPosts[i].id}" class="continue-btn">continue reading...</a>
       </div>
     </article>
     `;
-    //   if (i === 4) {
-    //     break;
-    //   }
-    // }
   }
 }
 renderCategoriezedPosts();
